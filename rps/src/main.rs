bb59f4c3d77c5c578c6a62bea72ee2c70933c928
Jerry Min <jerryjmin7@gmail.com>
2025-01-15 23:41:21 +0000

    experimenting random

--- rps/src/main.rs
+++ rps/src/main.rs
@@ -1,15 +1,27 @@
+use rand::Rng;
+use rand::prelude::*;
+use rand::distributions::WeightedIndex;
+
 struct RpsBot {
     strategy: [i32; 3],
 }
 
 impl RpsBot {
-    fn make_start() -> RpsBot {
-        RpsBot{strategy: [1, 1, 1]}
+    fn get_move(&self) -> i32 {
+        // [0, 1, 2][self.sample(&mut )]
+        0
     }
 }
 
+fn make_start() -> RpsBot {
+    RpsBot{strategy: [1, 1, 1]}
+}
 
 fn main() {
     println!("Hello, world!");
     let a = make_start();
+    let weights = [1, 2, 3];
+    let d = WeightedIndex::new(&weights).unwrap();
+    let mut rng = thread_rng();
+    println!("{:?}", d.sample(&mut rng));
 }
