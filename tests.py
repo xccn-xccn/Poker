df3c426cd8ae22a33ac21380a8d9a307295d8dc9
Jerry Min <jerryjmin7@gmail.com>
2025-02-09 15:39:12 +0000

    Experiment

--- tests.py
+++ tests.py
@@ -1,4 +1,20 @@
-import poker
+import test_mod
+import random
+from time import perf_counter
+from collections import Counter
 
-result = poker.count_words("smoooodles aa")
-print(result)
+start = perf_counter()
+
+string = "smoooodles aa"
+for r in range(100_000):
+    result = test_mod.count_words(string)
+
+# for r in range(100_000):
+#     result = Counter(string)
+
+# n = 10_000
+# l = [random.randint(0, n) for _ in range(n)]
+
+# print(list(sorted(l)))
+
+print(f"Time taken: {(perf_counter() - start) *1000} miliseconds")
