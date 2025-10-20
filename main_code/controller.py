ca1aff4d005629b48bd9e94f9d4b163d0f7ed642
Jerry Min <jerryjmin7@gmail.com>
2025-10-20 01:06:04 +0100

    Began big changes

---
+++ main_code/controller.py
@@ -0,0 +1,52 @@
+"""
+GUI should only call these methods
+
+    controller.start_hand()          → start a new round
+    controller.perform_action(a, val)  → player performs action (fold, call, raise)
+    controller.update()              → advance bot turns / round logic
+    controller.get_state()           → return dict of data for GUI rendering
+"""
+
+from poker import Table, Human, Bot, start
+
+class GameController:
+    def __init__(self):
+        self.table = start() if callable(start) else Table()
+        self.human_player_id = next(p.id for p in self.table.players if isinstance(p, Human))
+
+    def start_hand(self):
+        self.table.start_hand()
+
+    def perform_action(self, action: str, amount: int = 0):
+        self.table.single_move(({"fold": 1, "call": 2, "raise": 3}[action], amount))
+
+    def update(self):
+        if not self.table.running:
+            return
+        cont, end = self.table.start_move()
+        if cont:
+            player = self.table.current_player
+            if isinstance(player, Bot):
+                move = player.get_action(self.table)
+                self.table.single_move(move)
+        if end:
+            self.table.end_round()
+
+    def get_state(self):
+        return {
+            "players": [
+                {
+                    "id": p.id,
+                    "name": p.position_name,
+                    "chips": p.chips,
+                    "folded": p.fold,
+                    "hole_cards": p.hole_cards,
+                }
+                for p in self.table.players
+            ],
+            "community": self.table.community,
+            "pot": self.table.get_pot(),
+            "running": self.table.running,
+            "round": self.table.r,
+            "last_bet": self.table.last_bet,
+        }
