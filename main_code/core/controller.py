554bc0265de719168f5620003747f916b45f3c63
Jerry Min <jerryjmin7@gmail.com>
2025-10-28 17:40:47 +0000

    controller gets cards correctly

--- main_code/core/controller.py
+++ main_code/core/controller.py
@@ -3,17 +3,20 @@ GUI should only call these methods
 
     controller.start_hand()          → start a new round
     controller.perform_action(a, val)  → player performs action (fold, call, raise)
-    controller.update()              → Makes a bot move or if the current player cannot make a move, skips their turn 
+    controller.update()              → Makes a bot move or if the current player cannot make a move, skips their turn
     controller.get_state()           → return dict of data for GUI rendering
 """
 
 from core.poker import Table, Human, Bot, start
 
+
 class GameController:
     def __init__(self):
         self.create_table()
-        #TODO why is next used should probably be a list
-        self.human_player_id = next(p.id for p in self.table.players if isinstance(p, Human))
+        # TODO why is next used should probably be a list
+        self.human_player_id = next(
+            p.id for p in self.table.players if isinstance(p, Human)
+        )
 
     def create_table(self):
         self.table = start() if callable(start) else Table()
@@ -22,10 +25,9 @@ class GameController:
         self.table.start_hand()
 
     def perform_action(self, action: str, amount: int = 0):
-        #what if it's not human player move
-        #TODO needs to ensure start_move is called before
+        # what if it's not human player move
+        # TODO needs to ensure start_move is called before
         self.table.single_move(({"fold": 1, "call": 2, "raise": 3}[action], amount))
-
 
     def update(self):
         if not self.table.running:
@@ -36,18 +38,31 @@ class GameController:
             if isinstance(player, Bot):
                 move = player.get_action(self.table)
                 end = self.table.single_move(move)
-        if end: #NOTE doesn't call end_round within end_move to allow gui to display stuff
-            #TODO allow gui to display stuff maybe create a pygame event
+        if (
+            end
+        ):  # NOTE doesn't call end_round within end_move to allow gui to display stuff
+            # TODO allow gui to display stuff maybe create a pygame event
             self.table.end_round()
+
+    def get_cards(self, player):
+        if player.fold:
+            return None
+        if (
+            isinstance(player, Human)
+            or self.table.r >= self.table.skip_round
+            or self.table.players_remaining > 1
+            and self.table.running == False
+        ):
+            return player.hole_cards
+        return []
 
     def get_state(self):
         return {
             "players": [
                 {
-                    "id": p.id,
                     "chips": p.chips,
                     "folded": p.fold,
-                    "hole_cards": p.hole_cards,
+                    "hole_cards": self.get_cards(p),
                     "action": p.action,
                     "round_invested": p.round_invested,
                 }
@@ -58,6 +73,7 @@ class GameController:
             "running": self.table.running,
             "round": self.table.r,
         }
+
 
 class OnlineController:
     pass
